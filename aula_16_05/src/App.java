55628f2ad96bef12394c879500bd7dee63f8d418
Gabriel da Silva <gabriel.rodrigues106@fatec.sp.gov.br>
2025-05-16 14:13:10 +0000

    Implementa fila de veículos com operações de adição e remoção

--- aula_16_05/src/App.java
+++ aula_16_05/src/App.java
@@ -1,5 +1,60 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //Criar uma fila de veículos
+        Queue<String> fila_carros = new LinkedList<>();
+
+        //adicionar elementos com offer
+        //offer: ao inserir, é consultado a capacidade da fila, caso a fila esteja cheia, não é inserido
+        fila_carros.offer("Uno");
+        fila_carros.offer("Santana");
+        fila_carros.offer("Fusca");
+        fila_carros.offer("Gol G1");
+        fila_carros.offer("Corsa");
+        
+        //situação da fila
+        System.out.println("Situação: " + fila_carros);
+
+        //realizando o atendimento no pedágio
+        while(fila_carros.isEmpty()){
+            String carro_removido = fila_carros.poll();
+            System.out.println("Carro "+ carro_removido + " liberado no pedágio");
+        } 
+    }
+    public static void testeFila(){
+        //Criar uma fila de veículos
+        Queue<String> fila_carros = new LinkedList<>();
+
+        //adicionar elementos com offer
+        //offer: ao inserir, é consultado a capacidade da fila, caso a fila esteja cheia, não é inserido
+        fila_carros.offer("Uno");
+        fila_carros.offer("Santana");
+        fila_carros.offer("Fusca");
+
+        //add: não consulta o tamanho da fila, em razão disso, podera causar um overflow
+        fila_carros.add("Gol G1");
+        fila_carros.add("Corsa"); 
+
+        //quantos carros tem na fila
+        System.out.println("Quantidade de carros na fila: " + fila_carros.size());
+
+        //visualizar os elementos da fila
+        for (String carro : fila_carros){
+            System.out.println(carro);
+        }
+        //primeiro da fila
+        System.out.println("Primeiro da fila: " + fila_carros.peek());
+
+        //ver todos
+        System.out.println("Fila: " + fila_carros);
+
+        //remover o primeiro veículo
+        String car = fila_carros.poll();
+        System.out.println("Carro removido: " + car);
+
+        //situação da fila
+        System.out.println("Situação: " + fila_carros);
     }
 }
