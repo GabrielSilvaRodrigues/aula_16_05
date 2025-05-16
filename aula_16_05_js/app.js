ed902f1e77626117ab2f59c82511276c0a3f0a7f
Gabriel da Silva <gabriel.rodrigues106@fatec.sp.gov.br>
2025-05-16 15:37:01 +0000

    Implementa a lógica de criação e manipulação de uma fila de veículos com prioridade

--- aula_16_05_js/app.js
+++ aula_16_05_js/app.js
@@ -1,1 +1,23 @@
-console.log("Hello World");
+//criar uma fila
+let carro=[];
+
+//adicionar veículos: FIFO
+carro.push("Uno")
+carro.push("Gol")
+carro.push("Santana")
+
+//visualizar a fila
+console.log(carro)
+
+//remover
+carro.shift();
+
+console.log(carro)
+
+let fila_prioridade = [
+    {modelo: "Uno", prioridade: 2},
+    {modelo: "Gol", prioridade: 2},
+    {modelo: "Ambulância", prioridade: 1},
+    {modelo: "Policia", prioridade: 1}
+];
+fila_prioridade.sort((carro_a, carro_b) => carro_a.prioridade - carro_b.prioridade);
